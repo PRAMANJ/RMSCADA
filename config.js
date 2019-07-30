8f1124c80814687697e2bfce90da13ca41f12086
PRAMANJ <40769840+PRAMANJ@users.noreply.github.com>
2019-07-30 23:37:26 +0530

    PRWS files added

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-var publish_key='demo';
-var subscribe_key='demo';
+var publish_key='pub-c8ab4665-3e7b-4aaf-b6cd-ec98fc56202a';
+var subscribe_key='sub-a8e41c5c-f44f-11e1-9dd6-b97ea3affe3b';
 var simulate=true;
+var cycle_time=5;
+var DEFAULT_TAGSFILE="C:\\\\RMS\\\\tags.txt";
